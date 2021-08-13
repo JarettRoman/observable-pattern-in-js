1d25db1ff2e5e9d854d3feea705226175be02132
JarettRoman <afrocious@gmail.com>
2021-08-13 01:42:16 -0400

    init

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Observable Pattern in JS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jarettroman/pen/poPYdrm](https://codepen.io/jarettroman/pen/poPYdrm).
+
+Example of implementing an Observable pattern in Javascript.
---
+++ dist/script.js
@@ -0,0 +1,37 @@
+class MousePositionObservable {
+  constructor() {
+    this.subscriptions = [];
+    window.addEventListener("mousemove", this.handleMouseMove);
+  }
+
+  handleMouseMove = (e) => {
+    this.subscriptions.forEach((sub) => sub(e.clientX, e.clientY));
+  };
+
+  subscribe = (callback) => {
+    this.subscriptions.push(callback);
+
+    return () => {
+      this.subscriptions = this.subscriptions.filter((cb) => cb !== callback);
+    };
+  };
+}
+
+const mousePositionObservable = new MousePositionObservable();
+
+mousePositionObservable.subscribe((x, y) => {
+  const circle = document.querySelector(".circle");
+  window.setTimeout(() => {
+    circle.style.transform = `translate(${x}px, ${y}px)`;
+  }, 300);
+});
+
+mousePositionObservable.subscribe((x, y) => {
+  const board = document.querySelector(".mouse-position .position");
+  board.innerHTML = `
+    <div>
+      <div>ClientX: ${x}</div>
+      <div>ClientY: ${y}</div>
+    </div>
+  `;
+});
